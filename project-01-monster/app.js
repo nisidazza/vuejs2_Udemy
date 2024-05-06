6a33731e68165808bde19ab3272cc530e6523ade
Nisida Azzalini <nisida.azzalini@gmail.com>
2024-05-06 15:44:20 +0200

    logs battle messages

--- project-01-monster/app.js
+++ project-01-monster/app.js
@@ -8,6 +8,7 @@ Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   computed: {
@@ -49,21 +50,25 @@ Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      logMessages = [];
     },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomNumber(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomNumber(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomNumber(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -73,10 +78,18 @@ Vue.createApp({
       } else {
         this.playerHealth += healtValue;
       }
+      this.addLogMessage("player", "heal", healValue);
       this.attackPlayer();
     },
     surrender() {
       this.winner = "monster";
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 }).mount("#game");
